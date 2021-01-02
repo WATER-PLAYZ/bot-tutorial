902deb8ca083f1bb2c73d52b48477c922fb7402f
WATER-PLAYZ <74591650+WATER-PLAYZ@users.noreply.github.com>
2021-01-02 23:38:34 +0600

    Create meme.js

---
+++ meme.js
@@ -0,0 +1,37 @@
+
+ 
+
+const Discord = require('discord.js');
+const client = new Client();
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('message', async message => {
+  if (message.content === "ahe meme") {
+    const embed = new Discord.MessageEmbed()
+    got('https://www.reddit.com/r/memes/random/.json').then(response => {
+        let content = JSON.parse(response.body);
+        let permalink = content[0].data.children[0].data.permalink;
+        let memeUrl = `https://reddit.com${permalink}`;
+        let memeImage = content[0].data.children[0].data.url;
+        let memeTitle = content[0].data.children[0].data.title;
+        let memeUpvotes = content[0].data.children[0].data.ups;
+        let memeDownvotes = content[0].data.children[0].data.downs;
+        let memeNumComments = content[0].data.children[0].data.num_comments;
+//how the inside will look like
+        embed.setTitle(`${memeTitle}`)
+        embed.setURL(`${memeUrl}`)
+        embed.setImage(memeImage)
+        embed.setColor('RANDOM')
+        embed.setFooter(`👍 ${memeUpvotes} 👎 ${memeDownvotes} 💬 ${memeNumComments}`)
+            // Send the embed to the same channel as the message
+        message.channel.send(embed);
+    })
+  }
+});
+
+
+// Log our bot in using the token from https://discord.com/developers/applications
+client.login('your token here');
